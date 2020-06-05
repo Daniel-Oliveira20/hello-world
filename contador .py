f09a75365b906c0d3b3d0666f4cb6956354a173b
Daniel <daniel.apt@outlook.com>
2020-06-05 17:15:23 -0300

    Esse é um algoritimo de contador

---
+++ contador .py
@@ -0,0 +1,16 @@
+c = "s"
+while c == "s":
+    a = int(input("Quer contar de quanto: ")) 
+    b = int(input("Até quanto: "))
+    if a < b:
+        while a <= b:
+            print(a)
+            a = a+1
+    elif a > b:
+        while a >= b:
+            print(a)
+            a = a-1
+    else:
+        print("NUMEROS IGUAIS")
+    c = str(input("Deseja continuar? S/N: "))
+
