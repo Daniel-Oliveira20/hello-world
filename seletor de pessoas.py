47e8554ca46e89eea14929b080627bd709c0e92e
Daniel <daniel.apt@outlook.com>
2020-12-24 13:59:40 -0300

    algumas correções

--- seletor de pessoas.py
+++ seletor de pessoas.py
@@ -16,6 +16,7 @@ while verif != "n":
     elif sexo == "f" and idade >= 18 and cabelo == 4:
         contf = contf + 1
     verif = str(input("quer continuar? S/N: "))
-print("O número de homens com cabelos castanhos maior de idade é: ", contm )
-print("O número de mulheres com cabelos ruivos maior de idade é: ", contf )
+if verif == "n":
+    print("O número de homens com cabelos castanhos maior de idade é: ", contm )
+    print("O número de mulheres com cabelos ruivos maior de idade é: ", contf )
 
