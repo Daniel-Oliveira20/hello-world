e96cec7717337fe60d144c5100202f44d72602ee
Daniel <daniel.apt@outlook.com>
2021-01-24 22:11:16 -0300

    um pequeno acrecimo de comentario no exercicio 22

--- exe022.py
+++ exe022.py
@@ -1,3 +1,7 @@
+'''Crie um programa que leia o nome completo de uma pessoa e mostre:
+O nome com todas as letras em maiúsculo e minúsculos.
+Quantas letras ao todo (sem considerara espaços).
+Quantas letras tem o primeiro nome.'''
 n = str(input('digite seu nome completo: ')).strip()
 print('analisando seu nome...')
 print('seu nome em maiuscolos é: {} '.format(n.upper()))
